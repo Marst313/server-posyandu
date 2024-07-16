2c83bd29fc8237332c7bc9e8a96a8e168e6a9b01
Marst313 <dlegoinyoman1@gmail.com>
2024-07-16 09:29:01 +0700

    debug again

--- app.js
+++ app.js
@@ -36,16 +36,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.status(200).json({});
-  }
-  next();
-});
-
 // Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
